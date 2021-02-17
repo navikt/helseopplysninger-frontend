a808caeb768059c43dfea343bd5cd537e0b8f8c3
Michael Johansen <michael.johansen@nav.no>
2021-02-17 17:08:15 +0100

    fikser merge til main (#7)

---
+++ apps/hops-bestiller-backend/src/app/internals.ts
@@ -0,0 +1,17 @@
+import {Express} from "express";
+
+
+function internals(app: Express): void {
+    app.get('/internal/is-alive', (req, res) => {
+        res.send({message: 'Ready'});
+    });
+    app.get('/internal/is-ready', (req, res) => {
+        res.send({message: 'Ready'});
+    });
+    app.get('/internal/prometheus', (req, res) => {
+        res.send("not implemented");
+    });
+}
+
+
+export default internals;
