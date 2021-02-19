05b3f3f58336c1e721655eeee5c7e78ab9d89692
Michael Johansen <michael.johansen@nav.no>
2021-02-19 13:43:18 +0100

    enda en liten fix (#8)

--- apps/hops-bestiller-backend/src/app/hello-world.ts
+++ apps/hops-bestiller-backend/src/app/hello-world.ts
@@ -3,7 +3,11 @@ import {Express} from "express";
 
 function helloWorld(app: Express): void {
     app.get('/api', (req, res) => {
-        res.send({message: 'Welcome to hops-bestiller-backend!'});
+        res.send({
+            message: 'Welcome to hops-bestiller-backend!',
+            just: "doing a small update",
+            AZURE_APP_CLIENT_ID: process.env.AZURE_APP_CLIENT_ID
+        });
     });
 }
 
