139d1e8f47eec86a961b7d31263e854aa86fbd0c
Michael Johansen <michael.johansen@nav.no>
2021-03-22 19:37:25 +0100

    fjerne oppstartsfeil pa backend

--- apps/hops-bestiller-backend/src/database/connection.ts
+++ apps/hops-bestiller-backend/src/database/connection.ts
@@ -1,7 +1,20 @@
-import { Pool } from 'pg';
+import {Pool} from 'pg';
 import {database} from "../config.js"
+import logger from "../utils/logger";
 
 const dbPool = new Pool(database)
 
-
+dbPool.on('connect', () => {
+    dbPool.on('error', (error) => {
+        logger.error("(pg)" + error.name + ": " + error.message);
+    })
+});
+process.on("exit", async (code) => {
+    await dbPool.end()
+    logger.error("(pg) gracefully shutdown after exit("+code+")");
+})
+process.on('SIGTERM', async () => {
+    await dbPool.end();
+    logger.error("(pg) gracefully shutdown after SIGTERM");
+});
 export default dbPool;
--- apps/hops-bestiller-backend/src/database/run-migration.ts
+++ apps/hops-bestiller-backend/src/database/run-migration.ts
@@ -3,7 +3,6 @@ import dbPool from "./connection";
 
 async function runMigrations(migrationsDirectory: string) {
     const client = await dbPool.connect();
-
     await migrate({client}, migrationsDirectory)
 }
 
