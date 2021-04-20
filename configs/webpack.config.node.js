8feac1df113188d41e0793fb472a366079ee1f77
Michael Johansen <michael.johansen@nav.no>
2021-04-20 18:45:03 +0200

    utvider fhir strukturen

--- configs/webpack.config.node.js
+++ configs/webpack.config.node.js
@@ -2,7 +2,7 @@
 
 module.exports = (config, context) => {
   config.externals = config.externals || [];
-  config.externals.push("/node_modules/",'bufferutil', 'utf-8-validate');
+  config.externals.push('bufferutil', 'utf-8-validate', 'express', 'postgres-migrations', 'keyv');
   console.log('before', config);
   return config;
 };
