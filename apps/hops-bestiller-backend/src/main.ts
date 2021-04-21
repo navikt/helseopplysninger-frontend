e8443ba7d9a9c1894fe59e4c754b46f0ff066033
Michael Johansen <michael.johansen@nav.no>
2021-04-21 14:11:53 +0200

    utvider fhir strukturen

--- apps/hops-bestiller-backend/src/main.ts
+++ apps/hops-bestiller-backend/src/main.ts
@@ -14,13 +14,12 @@ import waitOn from "wait-on";
 import {kafkaConsumer} from "./kafka/kafka-consumer";
 import {kafkaClient} from "./kafka/kafka-client";
 import bestill from "./routes/bestill";
-import {attatchWsServer, wsBroadcast, wsServer} from "./ws/wsServer";
-
+import {attatchWsServer, wsBroadcast} from "./ws/wsServer";
 async function bootstrap(): Promise<Server> {
     logger.info("Bootstrap started");
     kafkaConsumer(
         kafkaClient,
-        "bestiller",
+        "bestiller2",
         kafkaTopics.bestillinger,
         message => {
             logger.info("KafkaReceivedMessage", message.value.toString());
