928cdbc2ed4020ca1784379e6c1a294168af0f23
Michael Johansen <michael.johansen@nav.no>
2021-04-21 10:53:18 +0200

    utvider fhir strukturen

--- configs/webpack.config.hops.js
+++ configs/webpack.config.hops.js
@@ -2,9 +2,6 @@
 const hopsBestillerApi = require('./mocks/hops-bestiller-api');
 const injectMock = require('./utils/inject-mock');
 module.exports = (config, context) => {
-  config.externals.push('bufferutil', 'utf-8-validate');
-  console.log('before', config);
   const after = injectMock(config, hopsBestillerApi);
-  console.log('after', after);
   return after;
 };
--- configs/webpack.config.node.js
+++ configs/webpack.config.node.js
@@ -3,6 +3,5 @@
 module.exports = (config, context) => {
   config.externals = config.externals || [];
   config.externals.push('bufferutil', 'utf-8-validate', 'express', 'postgres-migrations', 'keyv');
-  console.log('before', config);
   return config;
 };
