8a9caf2881de76747bffa0b0a8683d9800d32b6a
Michael Johansen <michael.johansen@gmail.com>
2021-07-27 19:28:01 +0200

    generaliserer deploy ytterligere

--- apps/sof-questionnaire/src/main.tsx
+++ apps/sof-questionnaire/src/main.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/App';
+import { Undertittel } from 'nav-frontend-typografi';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <small>change</small>
-            <App/>
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <BrowserRouter>
+      <Undertittel>change</Undertittel>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
