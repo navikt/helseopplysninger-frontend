dac738c79f3ac06cd9059a97f6280013bd09d1c7
Michael Johansen <michael.johansen@gmail.com>
2021-09-01 22:15:33 +0200

    refaktorerer fkr clienten

--- tools/test-data.ts
+++ tools/test-data.ts
@@ -1,29 +1,19 @@
-import {loadEnv} from "./utils/func";
-import {fkrGetPatient} from "@navikt/fkr-client";
-import {writeFileSync} from "fs";
-import {fileExists} from "@nrwl/workspace/src/utils/fileutils";
-import {join} from "path";
+import { loadEnv } from './utils/func';
+import { fkrGetPatient } from '@navikt/fkr-client';
+import { writeFileSync } from 'fs';
+import { fileExists } from '@nrwl/workspace/src/utils/fileutils';
+import { join } from 'path';
 
 loadEnv();
 
-const config = {
-    "fhirUrl": process.env.FKR_FHIR_URL,
-    "stsUrl": process.env.FKR_STS_URL,
-    "clientId": process.env.FKR_CLIENT_ID,
-    "accessKey": process.env.FKR_CLIENT_SECRET,
-}
-console.log(config)
-fkrGetPatient(config).then(patients => {
-    if (!fileExists(join(process.cwd(), "package.json"))) {
-        console.warn("Exiting script, not in project root directory")
-        process.exit(-1)
-    }
-    const patientsFolder = join(process.cwd(), "examples", "fkr");
-    writeFileSync(join(patientsFolder, "patients.json"), JSON.stringify(patients), "utf-8")
-    patients.forEach(patient => {
-        console.log(patient.id)
-    })
-})
-
-
-
+fkrGetPatient().then((patients) => {
+  if (!fileExists(join(process.cwd(), 'package.json'))) {
+    console.warn('Exiting script, not in project root directory');
+    process.exit(-1);
+  }
+  const patientsFolder = join(process.cwd(), 'examples', 'fkr');
+  writeFileSync(join(patientsFolder, 'patients.json'), JSON.stringify(patients), 'utf-8');
+  patients.forEach((patient) => {
+    console.log(patient.id);
+  });
+});
